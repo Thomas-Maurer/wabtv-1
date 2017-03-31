102b2f712b5eaf5f3c49851ad96d22e021dc2d71
thomas maurer <thomas.wellan@gmail.com>
2017-03-31 22:02:50 +0000

    add better admin

--- api/index.php
+++ api/index.php
@@ -5,8 +5,32 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $app = new \Slim\App();
-
+/*
 $app->add(new \Slim\Middleware\JwtAuthentication([
    "path" => ["/api", "/admin"],
     "secret" => "supersecretkeyyoushouldnotcommittogithub"
 ]));
+*/
+
+$app->get('/getCurrentProg', function (Request $request, Response $response)
+{
+  $prog = file_get_contents('data/prog.json');
+
+  $newResponse = $response->withJson(json_decode($prog));
+
+  return $newResponse;
+});
+
+$app->post('/authenticate', function (Request $request, Response $response)
+{
+  $body = $request->getBody();
+
+  $newResponse = $response->withJson($body);
+
+  return $newResponse;
+});
+
+
+
+
+$app->run();
--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ inject = require('gulp-inject');
 gulp.task('index', function () {
   var target = gulp.src('./index.html');
   // It's not necessary to read the files (will speed up things), we're only after their paths:
-  var sources = gulp.src(['./apps.js', './controllers/**/*.js', './css/**/*-layout.css'], {read: false});
+  var sources = gulp.src(['bower_components/angular/angular.js','node_modules/angular-ui-router/release/angular-ui-router.js' , './apps.js', './js/**/*.js', './css/**/*-layout.css'], {read: false});
 
   return target.pipe(inject(sources))
-    .pipe(gulp.dest('./build'));
+    .pipe(gulp.dest('./'));
 });
